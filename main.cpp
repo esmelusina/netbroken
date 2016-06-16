8702739a0504d0e9319deb10fbf045db6cc1763c
esmelusina <esmelusina@gmail.com>
2016-06-15 22:47:43 -0700

    factored raw data into packet struct. objectified addresses a little bit.

--- main.cpp
+++ main.cpp
@@ -1,79 +1,23 @@
 
-
 #include "netbroke.h"
 
-// Refactored to hide WSA stuff and use some objects
+
 int main(int cargs, char *vargs[])
 {
     nsfw::initNetworking();
 
     nsfw::Socket mysocket;
-
     mysocket.open(50000);
+ 
+    nsfw::Address out_addr(127,0,0,1,50000);
+    nsfw::Address in_addr;
 
-    sockaddr_in output_addr = nsfw::stoa("127.0.0.1:50000");
-    sockaddr_in input_addr;
+    nsfw::Packet out_pack {0,0, "Dinosaurs!"};
+    nsfw::Packet in_pack;
     
-    char out_buffer[40] = "Dino";
-    char in_buffer[40];
+    mysocket.send(out_pack, out_addr );
+    mysocket.recv( in_pack, in_addr );
 
-    mysocket.send(out_buffer, 40, output_addr);
-    mysocket.recv( in_buffer, 40, input_addr );
-
+    mysocket.close();
     nsfw::termNetworking();
 }
-
-// For Reference
-/*
-int main(int cargs, char *vargs[])
-{
-    WSAData data;
-    WSAStartup(MAKEWORD(2, 2), &data); // failed if != NO_ERROR
-
-    int handle = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // failed on <= 0
-   
-    sockaddr_in addr; // Internet address intended for use
-                      // With Sockets
-
-    //addr.sin_family           = AF_INET;
-    //addr.sin_addr.S_un.S_addr = INADDR_ANY;
-    //addr.sin_port             = htons(50000);
-
-    addr = nsfw::stoa("0.0.0.0:50000");
-
-    bind(handle, (sockaddr*)&addr, sizeof(sockaddr_in)); // failed on < 0
-    
-    DWORD param = 1;
-    ioctlsocket(handle, FIONBIO, &param);   // non-blocking! fails on != 0
-
-    sockaddr_in out_addr, in_addr;
-    int in_len = sizeof(sockaddr_in);
-    char out_buffer[40] = "I see you!"; // packet is 40 bytes!
-    char  in_buffer[40];
-
-    // targeting my local host
-    //out_addr.sin_family = AF_INET;
-    //out_addr.sin_addr.S_un.S_un_b.s_b1 = 127; // 10.15.22.54
-    //out_addr.sin_addr.S_un.S_un_b.s_b2 = 0;
-    //out_addr.sin_addr.S_un.S_un_b.s_b3 = 0;
-    //out_addr.sin_addr.S_un.S_un_b.s_b4 = 1;
-    //out_addr.sin_port = htons(50000);
-
-    out_addr = nsfw::stoa("127.0.0.1:50000");
-
-    // Send packet immediately to ourself
-    sendto(handle, out_buffer, 40, 0, (sockaddr*)&out_addr, sizeof(sockaddr_in));   // failed to send if retval != packet_size
-    recvfrom(handle,  in_buffer, 40, 0, (sockaddr*)& in_addr, &in_len);             // no data if retval <= 0
-   
-    // Echo server- sends out_buffer to anybody that sends any data to us
-    //while (true)
-    //{
-    //    while (recvfrom(handle,  in_buffer, 40, 0, (sockaddr*)&in_addr, &in_len) > 0)
-    //             sendto(handle, out_buffer, 40, 0, (sockaddr*)&in_addr, sizeof(sockaddr_in));
-    //}
-
-    closesocket(handle);
-    WSACleanup();
-    return 0;
-}
-*/
--- netbroke.h
+++ netbroke.h
@@ -5,8 +5,11 @@
 
 #include <WinSock2.h>   // for wsock32.lib
 #include <WS2tcpip.h>   // for ws2_32.lib
-
+#include <cstdio>
 #include <cstring>
+
+#define NSFW_MAKELONG(a,b,c,d) (a << 24 | b << 16 | c << 8 | d)
+#define NSFW_SPLITLONG(a) { (char)((a & 0xFF000000) >> 24), (char)((a & 0x00FF0000) >> 16), (char)((a & 0x0000FF00) >> 8), (char)(a & 0x000000FF)  }
 
 namespace nsfw
 {
@@ -17,35 +20,51 @@ namespace nsfw
     //convert string of format: "127.1.1.0:50000" to sockaddr_in
     sockaddr_in stoa(const char *src)
     {        
-        sockaddr_in dst;
-
-        union { char values[4]; unsigned ui; } IP; // this solves endianess
-        unsigned short port;
-        
-        const char *ptr = src; 
-        // Many ways to do this!
-        IP.values[0] = atoi(ptr);   ptr += strcspn(ptr, ".") + 1;
-        IP.values[1] = atoi(ptr);   ptr += strcspn(ptr, ".") + 1;
-        IP.values[2] = atoi(ptr);   ptr += strcspn(ptr, ".") + 1;
-        IP.values[3] = atoi(ptr);   ptr += strcspn(ptr, ":") + 1;        
-        port         = atoi(ptr);
-        
-        dst.sin_family = AF_INET;   //IPv4 -options include bluetooth, IR, etc.
-        dst.sin_addr.S_un.S_addr = IP.ui; // IP address as single integer
-        dst.sin_port = htons(port); //Port number. lookup MSB vs LSB!
+        sockaddr_in dst;        
+        dst.sin_family = AF_INET;  
+        unsigned char *qtr = (unsigned char*)&dst.sin_addr.S_un.S_addr;
+        sscanf_s(src,"%hhu.%hhu.%hhu.%hhu:%hu",qtr,qtr+1,qtr+2,qtr+3,&dst.sin_port);
+                
+        dst.sin_port = htons(dst.sin_port);
         return dst;
     }
 
-    struct Packet
+    class Address
     {
-        unsigned header;
-        float timestamp;
-        char bytes[1500];
+        // host format
+        unsigned long  addr;
+        unsigned short port; 
+    public:
+        Address() : addr(0), port(0) {}
+        Address(char a, char b, char c, char d, unsigned short p)
+            : port{p}, addr(NSFW_MAKELONG(a,b,c,d)) {}
+
+        Address(const char *ip) : Address(stoa(ip)){}
+        Address(const sockaddr_in &addr)
+            : port{ntohs(addr.sin_port)}, addr{ntohl(addr.sin_addr.S_un.S_addr)} {}
+    
+        sockaddr_in toN() const
+        {
+            sockaddr_in dst;
+            dst.sin_family = AF_INET;
+            dst.sin_addr.S_un.S_addr = htonl(addr);
+            dst.sin_port = htons(port);
+            return dst;
+        }
+        void toString(char *dst, size_t dlen) const
+        {
+            char blah[4] = NSFW_SPLITLONG(addr);
+            sprintf_s(dst,dlen,"%hhu.%hhu.%hhu.%hhu:%hu",blah[3],blah[2],blah[1],blah[0],port);
+        }                
     };
 
-    /*
-        Manages communication over IPv4.
-    */
+    struct Packet
+    {
+        unsigned long long MS;
+        unsigned long ID;
+        char data[1500];
+    };
+
     class Socket
     {
         int handle = 0;
@@ -76,16 +95,31 @@ namespace nsfw
         }
 
         // Packet, address
-        bool send(const char *data, int dlen, const sockaddr_in &addr)
+        bool sendRaw(const char *data, int dlen, const sockaddr_in &addr)
         {
             return sendto(handle, data, dlen, 0, (sockaddr*)&addr, sizeof(sockaddr_in)) == dlen;
         }
-        bool recv(char *data, int dlen, sockaddr_in &addr)
+        bool recvRaw(char *data, int dlen, sockaddr_in &addr)
         {
             int in_len = sizeof(sockaddr_in);
             return recvfrom(handle, data, dlen, 0, (sockaddr*)& addr, &in_len) == dlen;
         }
-    };
+ 
+
+        bool send(const Packet &pack, const Address &addr)
+        {
+            return sendto(handle, (char*)&pack, sizeof(Packet), 0, (sockaddr*)&(addr.toN()), sizeof(sockaddr_in)) == sizeof(Packet);
+        }
+        bool recv(Packet &pack, Address &addr)
+        {
+            int in_len = sizeof(sockaddr_in);
+            sockaddr_in t;
+            int bytes = recvfrom(handle, (char*)&pack, sizeof(Packet), 0, (sockaddr*)&t, &in_len);
+            addr = Address(t);
+            return bytes == sizeof(Packet);
+        }
+ 
+   };
 }
 
 //sendto(handle, out_buffer, 40, 0, (sockaddr*)&out_addr, sizeof(sockaddr_in));   // failed to send if retval != packet_size
