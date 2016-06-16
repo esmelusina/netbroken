f71ce56ea1ae511e222d4e48c0c25ef28be62390
esmelusina <esmelusina@gmail.com>
2016-06-16 12:21:31 -0700

    adjustments

--- main.cpp
+++ main.cpp
@@ -3,28 +3,33 @@
 #include "netbroke.h"
 #include <iostream>
 
-
 void main()
 {
     sfwl::initContext();
     nsfw::initNetworking();
 
-    ShipInput si1;
-    Ship      ship1;
-    Player    player1;
+    ShipInput  input1,  input2;
+    Ship        ship1,   ship2;
+    Player    player1, player2;
+
+    ship2.color = CYAN;
 
     while (sfwl::stepContext())
     {
         float dt = sfwl::getDeltaTime();
 
-        si1 = player1.getInput(ship1);
-        ship1.update(dt, si1);
+        input1 = player1.getInput(ship1);
+        input2 = player2.getInput(ship2);
+
+        ship1.update(dt, input1);
+        ship2.update(dt, input2);
+
         ship1.draw();
+        ship2.draw();
 
         Particle::updateAll(dt);
         Particle::drawAll();
     }
-
 
     sfwl::termContext();
     nsfw::termNetworking();
