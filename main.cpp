73e5a3eddfc97896b595749babb6d9582abc40c5
esmelusina <esmelusina@gmail.com>
2016-06-15 18:03:21 -0700

    socket object bugfix,
    NOTE: no error checking or handling currently in place.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 
 
-#pragma comment( lib, "wsock32.lib" ) // windows top-level wrapper for winsock - 'WSA' functions
-#pragma comment( lib, "ws2_32.lib"  ) // traditional socket libraries
-
-#include <iostream>
-#include <WinSock2.h>   // for wsock32.lib
-#include <WS2tcpip.h>   // for ws2_32.lib
-
 #include "netbroke.h"
 
+// Refactored to hide WSA stuff and use some objects
 int main(int cargs, char *vargs[])
 {
-    auto d = nsfw::stoa("126.0.0.1:234", 14);
+    nsfw::initNetworking();
 
+    nsfw::Socket mysocket;
 
+    mysocket.open(50000);
+
+    sockaddr_in output_addr = nsfw::stoa("127.0.0.1:50000");
+    sockaddr_in input_addr;
+    
+    char out_buffer[40] = "Dino";
+    char in_buffer[40];
+
+    mysocket.send(out_buffer, 40, output_addr);
+    mysocket.recv( in_buffer, 40, input_addr );
+
+    nsfw::termNetworking();
+}
+
+// For Reference
+/*
+int main(int cargs, char *vargs[])
+{
     WSAData data;
     WSAStartup(MAKEWORD(2, 2), &data); // failed if != NO_ERROR
 
@@ -22,10 +35,12 @@ int main(int cargs, char *vargs[])
     sockaddr_in addr; // Internet address intended for use
                       // With Sockets
 
-    addr.sin_family           = AF_INET;
-    addr.sin_addr.S_un.S_addr = INADDR_ANY;
-    addr.sin_port             = htons(50000);
-    
+    //addr.sin_family           = AF_INET;
+    //addr.sin_addr.S_un.S_addr = INADDR_ANY;
+    //addr.sin_port             = htons(50000);
+
+    addr = nsfw::stoa("0.0.0.0:50000");
+
     bind(handle, (sockaddr*)&addr, sizeof(sockaddr_in)); // failed on < 0
     
     DWORD param = 1;
@@ -37,12 +52,14 @@ int main(int cargs, char *vargs[])
     char  in_buffer[40];
 
     // targeting my local host
-    out_addr.sin_family = AF_INET;
-    out_addr.sin_addr.S_un.S_un_b.s_b1 = 127; // 10.15.22.54
-    out_addr.sin_addr.S_un.S_un_b.s_b2 = 0;
-    out_addr.sin_addr.S_un.S_un_b.s_b3 = 0;
-    out_addr.sin_addr.S_un.S_un_b.s_b4 = 1;
-    out_addr.sin_port = htons(50000);
+    //out_addr.sin_family = AF_INET;
+    //out_addr.sin_addr.S_un.S_un_b.s_b1 = 127; // 10.15.22.54
+    //out_addr.sin_addr.S_un.S_un_b.s_b2 = 0;
+    //out_addr.sin_addr.S_un.S_un_b.s_b3 = 0;
+    //out_addr.sin_addr.S_un.S_un_b.s_b4 = 1;
+    //out_addr.sin_port = htons(50000);
+
+    out_addr = nsfw::stoa("127.0.0.1:50000");
 
     // Send packet immediately to ourself
     sendto(handle, out_buffer, 40, 0, (sockaddr*)&out_addr, sizeof(sockaddr_in));   // failed to send if retval != packet_size
@@ -59,3 +76,4 @@ int main(int cargs, char *vargs[])
     WSACleanup();
     return 0;
 }
+*/
--- netbroke.h
+++ netbroke.h
@@ -10,27 +10,83 @@
 
 namespace nsfw
 {
+    // necessary prerequisites for working with windows networking
     bool initNetworking() { WSAData data; return WSAStartup(MAKEWORD(2, 2), &data) == 0; }
     bool termNetworking() {               return WSACleanup()                      == 0; }
 
-    //format: "127.1.1.0:50000"
-    sockaddr_in stoa(const char *src, size_t len)
-    {
-        
+    //convert string of format: "127.1.1.0:50000" to sockaddr_in
+    sockaddr_in stoa(const char *src)
+    {        
+        sockaddr_in dst;
+
         union { char values[4]; unsigned ui; } IP; // this solves endianess
         unsigned short port;
-        sockaddr_in dst;
+        
         const char *ptr = src; 
-
-        IP.values[0] = atoi(ptr); ptr += strcspn(ptr, ".") + 1;
-        IP.values[1] = atoi(ptr); ptr += strcspn(ptr, ".") + 1;
-        IP.values[2] = atoi(ptr); ptr += strcspn(ptr, ".") + 1;
-        IP.values[3] = atoi(ptr); ptr += strcspn(ptr, ":") + 1;
+        // Many ways to do this!
+        IP.values[0] = atoi(ptr);   ptr += strcspn(ptr, ".") + 1;
+        IP.values[1] = atoi(ptr);   ptr += strcspn(ptr, ".") + 1;
+        IP.values[2] = atoi(ptr);   ptr += strcspn(ptr, ".") + 1;
+        IP.values[3] = atoi(ptr);   ptr += strcspn(ptr, ":") + 1;        
         port         = atoi(ptr);
         
-        dst.sin_family = AF_INET;
-        dst.sin_addr.S_un.S_addr = IP.ui;
-        dst.sin_port = htons(port);
+        dst.sin_family = AF_INET;   //IPv4 -options include bluetooth, IR, etc.
+        dst.sin_addr.S_un.S_addr = IP.ui; // IP address as single integer
+        dst.sin_port = htons(port); //Port number. lookup MSB vs LSB!
         return dst;
     }
+
+    struct Packet
+    {
+        unsigned header;
+        float timestamp;
+        char bytes[1500];
+    };
+
+    /*
+        Manages communication over IPv4.
+    */
+    class Socket
+    {
+        int handle = 0;
+    public:
+        ~Socket() { close(); }
+        
+        bool isOpen() const { return handle > 0; }
+        
+        bool open(unsigned short port = 0)
+        {            
+            sockaddr_in addr;
+            addr.sin_family           = AF_INET;
+            addr.sin_addr.S_un.S_addr = INADDR_ANY;
+            addr.sin_port             = htons(port);            
+            DWORD param = 1;
+
+            handle = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); 
+            bind(handle, (sockaddr*)&addr, sizeof(sockaddr_in));
+            ioctlsocket(handle, FIONBIO, &param);
+            return true;
+        }
+        bool close()
+        {
+            if (!isOpen())
+            closesocket(handle);
+            handle = 0;
+            return true;
+        }
+
+        // Packet, address
+        bool send(const char *data, int dlen, const sockaddr_in &addr)
+        {
+            return sendto(handle, data, dlen, 0, (sockaddr*)&addr, sizeof(sockaddr_in)) == dlen;
+        }
+        bool recv(char *data, int dlen, sockaddr_in &addr)
+        {
+            int in_len = sizeof(sockaddr_in);
+            return recvfrom(handle, data, dlen, 0, (sockaddr*)& addr, &in_len) == dlen;
+        }
+    };
 }
+
+//sendto(handle, out_buffer, 40, 0, (sockaddr*)&out_addr, sizeof(sockaddr_in));   // failed to send if retval != packet_size
+//recvfrom(handle, in_buffer, 40, 0, (sockaddr*)& in_addr, &in_len);             // no data if retval <= 0
