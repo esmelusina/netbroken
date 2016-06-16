cd9772e6f8d43f790eb84a27b95ad319710c7380
esmelusina <esmelusina@gmail.com>
2016-06-16 12:19:08 -0700

    Initial Game Functionality

--- main.cpp
+++ main.cpp
@@ -6,5 +6,26 @@
 
 void main()
 {
+    sfwl::initContext();
+    nsfw::initNetworking();
 
+    ShipInput si1;
+    Ship      ship1;
+    Player    player1;
+
+    while (sfwl::stepContext())
+    {
+        float dt = sfwl::getDeltaTime();
+
+        si1 = player1.getInput(ship1);
+        ship1.update(dt, si1);
+        ship1.draw();
+
+        Particle::updateAll(dt);
+        Particle::drawAll();
+    }
+
+
+    sfwl::termContext();
+    nsfw::termNetworking();
 }
