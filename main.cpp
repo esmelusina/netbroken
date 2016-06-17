8b7bfbc26cf28757604a1c50c444fe1f4f30432a
esmelusina <esmelusina@gmail.com>
2016-06-16 17:22:31 -0700

    server and client

--- main.cpp
+++ main.cpp
@@ -9,29 +9,35 @@ void main()
 {
     sfwl::initContext();
     nsfw::initNetworking();
-    
+ 
+    bool server = true;
+
+    // Replaced with Arrays
     Player localPlayer;
     std::vector<Ship>                ships;
     std::vector<ShipInput>           inputs;
     std::unordered_map<unsigned long, size_t> connections;
 
+    // Initialize local player
     ships.emplace_back();
     inputs.emplace_back();
-
     ships[0].color = 0x7700FF;
 
     nsfw::Socket mysocket;
 
     mysocket.open(50000);
-    nsfw::Address inaddr, outaddr(10,15,22,61,50000);
-                                                        
-    struct PACKET { float time; ShipInput input; Ship state; };
+
+    nsfw::Address inaddr, outaddr(10,15,22,54,50000);                                   
+    unsigned long MYIP = outaddr.getIPH();
+    
+    connections[MYIP] = 0;
+    struct PACKET { unsigned long keyIP; float time; ShipInput input; Ship state; };
     PACKET in_data, out_data;
 
     while (sfwl::stepContext())
     {
         float dt = sfwl::getDeltaTime();
-        
+     // use loops now!   
         inputs[0] = localPlayer.getInput(ships[0]);
         
         for (size_t i = 0; i < ships.size(); ++i)
@@ -41,30 +47,39 @@ void main()
         }
 
         Particle::updateAll(dt);
-        Particle::drawAll();       
+        Particle::drawAll();                   
 
-
-        out_data = { sfwl::getTime(), inputs[0], ships[0] };       
-
-        mysocket.send((char*)&out_data, sizeof(PACKET), outaddr); // use input1!
-                
+        if (!server)
+        {
+            out_data = { 0, sfwl::getTime(), inputs[0], ships[0] };
+            mysocket.send((char*)&out_data, sizeof(PACKET), outaddr);
+        }
 
         while (mysocket.recv((char*)&in_data, sizeof(PACKET), inaddr))
-        {
-            mysocket.send((char*)&out_data, sizeof(PACKET), inaddr);
-            // Asset a new player!
-            if (connections.count(inaddr.getIPH()) == 0)
+        {         
+            unsigned long tIP = server? inaddr.getIPH() : in_data.keyIP;
+
+            if (connections.count(tIP) == 0)
             {
-                connections[inaddr.getIPH()] = ships.size();
+                connections[tIP] = ships.size();
                 ships.emplace_back();
                 inputs.emplace_back();
             }
 
-            size_t current = connections[inaddr.getIPH()];            
+            size_t current  = connections[tIP];            
             inputs[current] = in_data.input;
             ships[current]  = in_data.state;
+            
+            if(server)
+                for each(auto c in connections)
+                    if(c.first != tIP)
+                    {
+                        out_data = {c.first, sfwl::getTime(), inputs[c.second], ships[c.second]};
+                        mysocket.send((char*)&out_data, sizeof(PACKET), inaddr);
+                    }
         }
     }
+
 
     mysocket.close();
     sfwl::termContext();
